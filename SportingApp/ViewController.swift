b309e501e6f24204d527827bcc3bcf50fa04b61d
habiba <habiba@habibas-Mac.local>
2024-05-16 04:34:28 -0700

    first commit

---
+++ SportingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportingApp
+//
+//  Created by habiba on 5/16/24.
+//  Copyright © 2024 habiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
