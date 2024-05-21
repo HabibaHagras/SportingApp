7002ab5ba048e723f0d299ca552988af2b800e92
maha <maha@mahas-Mac.local>
2024-05-20 19:09:19 -0700

    add pods(SDWebImage,Alamofire,ReachabilitySwift

---
+++ SportingApp/Modules/League/ViewModle/ApiService.swift
@@ -0,0 +1,9 @@
+//
+//  ApiService.swift
+//  SportingApp
+//
+//  Created by maha on 5/20/24.
+//  Copyright © 2024 habiba. All rights reserved.
+//
+
+import Foundation
---
+++ SportingApp/Modules/League/ViewModle/EventsViewModle.swift
@@ -0,0 +1,12 @@
+//
+//  EventsViewModle.swift
+//  SportingApp
+//
+//  Created by maha on 5/20/24.
+//  Copyright © 2024 habiba. All rights reserved.
+//
+
+import Foundation
+class EventsViewModle{
+    
+}
