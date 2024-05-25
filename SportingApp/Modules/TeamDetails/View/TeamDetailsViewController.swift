410235dfd3cb09bc5aac6182ca25a9b004a02621
Maha Farghaly <mahafarghalymo@gmail.com>
2024-05-24 17:20:32 -0700

    implemnet ui for teams details

---
+++ SportingApp/Modules/TeamDetails/View/TeamDetailsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TeamDetailsViewController.swift
+//  SportingApp
+//
+//  Created by maha on 5/24/24.
+//  Copyright © 2024 habiba. All rights reserved.
+//
+
+import UIKit
+
+class TeamDetailsViewController: UIViewController {
+    @IBOutlet weak var teamImage: UIImageView!
+    
+    @IBOutlet weak var teamNameLB: UILabel!
+    
+    @IBOutlet weak var playersCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
